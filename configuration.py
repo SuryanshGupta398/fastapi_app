4e61c389305abe88cf76e5d57f55af88e4ab2b59
Suryansh Gupta <guptajisuryansh286@gmail.com>
2025-09-15 23:53:55 +0530

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -2,7 +2,7 @@ import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-MONGO_URI = os.getenv("mongodb+srv://user_01:bala@cluster0.qgcatm6.mongodb.net/fake_new_app?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URI = os.getenv("MONGO_URI")
 
 # Create a new client and connect to the server
 client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
